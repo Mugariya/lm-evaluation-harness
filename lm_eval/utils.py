bed969a470444a8216b54aa31883d2e53ef3ed23
EC2 Default User <ec2-user@ip-172-16-91-97.us-west-2.compute.internal>
2024-03-06 08:25:39 +0000

    added stuff in utils

--- lm_eval/utils.py
+++ lm_eval/utils.py
@@ -15,6 +15,21 @@ class ExitCodeError(Exception):
 def sh(x):
     if os.system(x):
         raise ExitCodeError()
+        
+class timeout:
+    def __init__(self, seconds=1, error_message="Timeout"):
+        self.seconds = seconds
+        self.error_message = error_message
+
+    def handle_timeout(self, signum, frame):
+        raise TimeoutError(self.error_message)
+
+    def __enter__(self):
+        signal.signal(signal.SIGALRM, self.handle_timeout)
+        signal.alarm(self.seconds)
+
+    def __exit__(self, type, value, traceback):
+        signal.alarm(0)
 
 
 def simple_parse_args_string(args_string):
